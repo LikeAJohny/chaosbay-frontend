3c5a118e6adffb8c92c7a79c9e94149cff8bd8f9
LikeAJohny <LikeAJohny@users.noreply.github.com>
2023-11-12 14:19:28 +0100

    remove output from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [tailwind({
     applyBaseStyles: true
   }), svelte()]
