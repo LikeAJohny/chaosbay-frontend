07d3d3dbabbffdbfe752ea8f1c2e38e226099ad3
LikeAJohny <LikeAJohny@users.noreply.github.com>
2023-11-15 15:41:21 +0100

    update project

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-  projectId: "8me5yfa8",
-  dataset: "dev",
+  projectId: "2vsqsxqy",
+  dataset: "production",
   useCdn: false,
   apiVersion: "2023-10-16",
   // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
